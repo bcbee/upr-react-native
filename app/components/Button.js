cdd2cffa1ae81483880c26d8e2372a0170b70e86
Brendan Boyle <brendan.boyle@epicgames.com>
2024-02-04 02:18:46 -0500

    Fix button styling

--- app/components/Button.js
+++ app/components/Button.js
@@ -1,15 +1,58 @@
-import { TouchableOpacity } from "react-native";
-import { Button } from "react-native";
+import { TouchableOpacity, StyleSheet } from "react-native";
+import { Text } from "react-native";
 import Icon from "react-native-vector-icons/Feather";
 
-export default function UPRButton({ title, icon, onPress, style }) {
+export default function UPRButton({
+  title,
+  icon,
+  onPress,
+  disabled,
+  style,
+  textStyle,
+}) {
   if (icon) {
     return (
       <TouchableOpacity onPress={onPress}>
-        <Icon name={icon} size={28} color="#FFF" style={style} />
+        <Icon name={icon} size={28} color="#FFF" />
       </TouchableOpacity>
     );
   }
 
-  return <Button title={title} onPress={onPress} style={style} />;
+  return (
+    <TouchableOpacity
+      onPress={onPress}
+      disabled={disabled}
+      style={[styles.button, disabled ? styles.disabled : undefined, style]}
+    >
+      <Text
+        style={[
+          styles.buttonText,
+          disabled ? styles.disabledText : undefined,
+          textStyle,
+        ]}
+      >
+        {title}
+      </Text>
+    </TouchableOpacity>
+  );
 }
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: "#B30298",
+    borderRadius: 5,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonText: {
+    color: "white",
+    fontFamily: "SugarcubesBold",
+    fontSize: 20,
+  },
+  disabled: {
+    backgroundColor: "#CCC",
+  },
+  disabledText: {
+    color: "#000",
+  },
+});
